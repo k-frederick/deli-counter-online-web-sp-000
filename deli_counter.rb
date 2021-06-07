b39feb0729ad4abe13d860ee6ac702caae527a73
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-07 23:49:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+def line(array)
+  if katz_deli
+    puts "The line is currently empty."
+  elsif other_deli
+    for name in other_deli
+    puts "The line is currently:"
+  #else another_deli
+    #puts "The line is currently:"
+  #end
+  end
+  end
+end
+def take_a_number(katz_deli, "{name}")
+  #katz_deli.each_char { |name| puts  }
+end
+def now_serving
+end
