398f1f281b3b57c837e09fa8b762bd4e23b68a5f
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-08 00:08:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,19 +6,7 @@ def line(array)
   # elsif other_deli
   #   for name in other_deli
   #   puts "The line is currently:"
-    if katz_deli.length == 0
-      puts "The line is currently empty."
-    else
-      message="The line is currently:"
-
-      katz_deli.each_with_index do |value, index|
-        message += " #{index.to_i+1}. #{value}"
-      end
-
-      puts "#{message}"
-    end
-  end
-  end
+    
 end
 def take_a_number(katz_deli, name)
   katz_deli << name
@@ -31,4 +19,4 @@ def now_serving(katz_deli)
    puts "Currently serving #{katz_deli.shift}."
  end
 end
-end
+
