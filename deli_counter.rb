acbc92694ba5366860ea81f8c5c19432f18b5266
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-08 00:02:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 def now_serving(other_deli)
-  
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  end
 end
