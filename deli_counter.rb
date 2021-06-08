d0e4b502e6f1472d47e4c9eac4f1384c42fe802b
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-08 00:23:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,7 @@ def line(katz_deli)
 
     puts "#{message}"
   end
+end
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
